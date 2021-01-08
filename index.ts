041ed1f73decf2a4a8a41164c3f5ffff7fa0eed7
siyi98 <zhusijia1998@gmail.com>
2021-01-08 18:10:36 +0800

    完善useSafeEffect内部逻辑

--- index.ts
+++ index.ts
@@ -1,29 +1,28 @@
 import { useEffect } from 'react';
 
-const useSafeEffect = (effect, destroy, inputs) => {
-  const hasDestroy: boolean = typeof destroy === 'function';
+const useSafeEffect = (mount, ...args) => {
+  if (args.length && args.length > 2) {
+    console.error('此方法仅支持传入三个参数：1.mount函数 2.unMount函数 3.监听的入参');
+  }
+  const hasUnMount: boolean = typeof args[0] === 'function';
 
   useEffect(
     () => {
-      let result;
-      let mounted: boolean = true;
-      let maybePromise = effect(() => {
-        return mounted;
-      });
+      Promise.resolve(mount)
+        .then((value) => {
+          return value;
+        })
+        .catch((error) => {
+          return error;
+        });
 
-      Promise.resolve(maybePromise).then((value) => {
-        result = value;
-      });
-
-      return () => {
-        mounted = false;
-
-        if (hasDestroy) {
-          destroy(result);
-        }
-      };
+      if (hasUnMount) {
+        return () => {
+          args[0];
+        };
+      }
     },
-    hasDestroy ? inputs : destroy
+    hasUnMount ? args[1] : args[0]
   );
 };
 
