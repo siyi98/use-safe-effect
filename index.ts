683ae5d444e6dcb91f5adb045ac1be506e62a8bd
siyi98 <zhusijia1998@gmail.com>
2021-01-12 11:23:47 +0800

    添加mount&unMount注释

--- index.ts
+++ index.ts
@@ -1,24 +1,25 @@
 import { useEffect } from 'react';
 
-const useSafeEffect = (mount, ...args) => {
+const useSafeEffect = (mount: any, ...args: any[]) => {
   if (args.length && args.length > 2) {
-    console.error('此方法仅支持传入三个参数：1.mount函数 2.unMount函数 3.监听的入参');
+    console.error('`use-safe-effect`仅支持传入三个参数：1.mount函数 2.unMount函数 3.入参');
+    return false;
   }
   const hasUnMount: boolean = typeof args[0] === 'function';
-
   useEffect(
+    // componentDidMount
     () => {
-      Promise.resolve(mount)
+      Promise.resolve(mount())
         .then((value) => {
           return value;
         })
         .catch((error) => {
           return error;
         });
-
+      // componentWillUnMount
       if (hasUnMount) {
         return () => {
-          args[0];
+          args[0]();
         };
       }
     },
